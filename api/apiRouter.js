e9454b4b7e63be1b9e2964a2ab960e8fcd7f5ad2
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-21 07:34:11 -0800

    Create validateAuth middleware. Require and call it in apiRouter on authRouter.

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -1,15 +1,17 @@
 const router = require('express').Router();
 
-
+// Pull in Routers---------------------------------------
 const authRouter = require('../router/authRouter');
 const chefRouter = require('../router/chefRouter');
 
+// Pull in custom middleware-----------------------------
 const restricted = require('../middleware/restricted');
+const validateAuth = require('../middleware/validateAuth');
 
-router.use('/api/auth', authRouter)
+router.use('/api/auth', validateAuth, authRouter)
 router.use('/api/chef_portfolio', restricted, chefRouter);
 
-// Global test endpoint
+// Global test endpoint----------------------------------
 router.get('/', (req, res) => {
 res.send(`<h3>Let's Get Cooking!</h3>`)
 });
