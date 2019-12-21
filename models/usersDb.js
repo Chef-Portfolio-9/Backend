2e068a6ce6f49c9508ae9f6f5ad47ed53e6e2493
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-21 07:36:47 -0800

    Create usersDb.js in models folder.

---
+++ models/usersDb.js
@@ -0,0 +1,30 @@
+const db = require('../database/dbConfig');
+module.exports = {
+	get,
+	getBy,
+	add,
+	findById
+};
+
+function get() {
+	return db('users').select('id', 'username', 'department');
+}
+
+function getBy(username) {
+	return db('users')
+		.select('id', 'username', 'password')
+		.where(username);
+}
+
+function add(user) {
+	return db('users')
+		.insert(user, 'id')
+		.returning('id');
+}
+
+function findById(id) {
+	return db('users')
+		.select('id', 'username')
+		.where({ id })
+		.first();
+}
