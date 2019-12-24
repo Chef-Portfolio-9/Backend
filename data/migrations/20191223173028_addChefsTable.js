2b9e8391fa1f5906bf1aecb3c43378bc12f6a8ac
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-23 17:40:08 -0800

    Create db3 by running latest migration. Create Chefs table.

---
+++ data/migrations/20191223173028_addChefsTable.js
@@ -0,0 +1,24 @@
+exports.up = function(knex) {
+	return knex.schema.createTable('chefs', tbl => {
+		tbl.increments();
+
+		tbl
+			.string('username', 255)
+			.notNullable()
+			.unique();
+		tbl.string('password', 255).notNullable();
+		tbl.string('location', 255).notNullable();
+		tbl
+			.integer('role_id')
+			.unsigned()
+			.notNullable()
+			.references('id')
+			.inTable('roles')
+			.onDelete('RESTRICT')
+			.onUpdate('CASCADE');
+	});
+};
+
+exports.down = function(knex) {
+	return knex.schema.dropTableIfExists('chefs');
+};
