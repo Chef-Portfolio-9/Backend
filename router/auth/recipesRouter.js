8aa23ae230490a2b54cd1459f27de2b220e9d466
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-29 21:50:25 -0800

    Create Global GET for Recipes.

---
+++ router/auth/recipesRouter.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+//Pull in knex helper models
+const recipesDb = require('../../models/recipesDb');
+
+//Global GET
+router.get('/', (req, res) => {
+	recipesDb
+		.get()
+		.then(food => {
+			res.status(200).json(food);
+		})
+		.catch(err => {
+			res
+				.status(500)
+				.json({ message: 'There was an error getting the recipes.', err });
+		});
+});
+
+//GET by id
+router.get('/:id', (req, res) => {});
+
+//POST
+router.post('/', (req, res) => {});
+
+//PUT by id
+router.put('/:id', (req, res) => {});
+
+//DELETE by id
+router.delete('/:id', (req, res) => {});
+
+module.exports = router;
