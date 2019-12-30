71638473bb1b1e4cd5b81e0b77b1bed886754e59
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-29 22:29:56 -0800

    Require and invoke recipesRouter in apiRouter.

--- api/apiRouter.js
+++ api/apiRouter.js
@@ -3,13 +3,15 @@ const router = require('express').Router();
 // Pull in Routers---------------------------------------
 const authChefsRouter = require('../router/auth/authChefsRouter');
 const authUsersRouter = require('../router/auth/authUsersRouter');
+const recipesRouter = require('../router/recipesRouter');
 
 // Pull in custom middleware-----------------------------
-// const restricted = require('../middleware/restricted');
+const restricted = require('../middleware/restricted');
 const validateAuth = require('../middleware/validateAuth');
 
-router.use('/api/auth/chefs', validateAuth, authChefsRouter)
+router.use('/api/auth/chefs', validateAuth, authChefsRouter);
 router.use('/api/auth/users', validateAuth, authUsersRouter);
+router.use('/api/recipes', restricted, recipesRouter)
 
 // Global test endpoint----------------------------------
 router.get('/', (req, res) => {
