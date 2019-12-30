46721db380d81fb214eb0630c1628c0b36793457
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-29 22:23:52 -0800

    Create DELETE by id for recipes.

--- router/recipesRouter.js
+++ router/recipesRouter.js
@@ -69,6 +69,26 @@ router.put('/:id', (req, res) => {
 });
 
 //DELETE by id
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+	const id = req.params.id;
+
+	recipesDb
+		.getById(id)
+		.then(deletedRecipe => {
+			recipesDb
+				.remove(id, deletedRecipe)
+				.then(gone => {
+					res
+						.status(200)
+						.json({ message: `Recipe id: ${id} was deleted.`, deletedRecipe });
+				})
+				.catch(err => {
+					res.status(500).json({ message: 'Error deleting the recipe.', err });
+				});
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Error finding that recipe.', err });
+		});
+});
 
 module.exports = router;
