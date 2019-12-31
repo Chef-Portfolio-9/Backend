9d2968b803645724569c19bf7bc18a9874d87c0f
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-12-30 17:25:04 -0800

    Remove global validate middleware.

--- middleware/validateId.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-	const id = req.params.id;
-
-	if (!id) {
-		res.status(400).json({ message: 'Missing valid id!' });
-	} else {
-		next();
-	}
-};
--- middleware/validatePost.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-    const data = req.body
-
-    if (!data) {
-        res.status(400).json({ message: "Missing needed data." })
-    } else {
-        next();
-    }
-}
