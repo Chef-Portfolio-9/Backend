5d2d97f5deb5ec8f78e7a9d5013f0241ef6a2c0f
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-01-04 11:01:17 -0800

    Change instructionTable migration.

--- data/migrations/20191224071033_addInstructionsTable.js
+++ data/migrations/20191224071033_addInstructionsTable.js
@@ -1,15 +1,7 @@
 exports.up = function(knex) {
 	return knex.schema.createTable('instructions', tbl => {
 		tbl.increments();
-		tbl
-			.integer('recipe_id')
-			.unsigned()
-			.notNullable()
-			.references('id')
-			.inTable('recipes')
-			.onUpdate('CASCADE')
-			.onDelete('RESTRICT');
-		tbl.integer('step_number').notNullable();
+		
 		tbl.string('instruction', 1000).notNullable();
 	});
 };
