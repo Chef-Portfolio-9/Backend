33b0155dfb5fb48073513c9d310f43ffb80e74cd
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-01-04 11:11:00 -0800

    Create instruction seeds.

--- data/seeds/006-instructions.js
+++ data/seeds/006-instructions.js
@@ -1,15 +1,30 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('instructions').truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex('instructions').insert([
-        {step_number: '', instruction: ''},
-        {step_number: '', instruction: ''},
-        {step_number: '', instruction: ''},
-        {step_number: '', instruction: ''},
-        {step_number: '', instruction: ''}
-      ]);
-    });
+	// Deletes ALL existing entries
+	return knex('instructions')
+		.truncate()
+		.then(function() {
+			// Inserts seed entries
+			return knex('instructions').insert([
+				{
+					instruction:
+						'Marinate Chickpeas for 24hours in Olive Oil, Cumin, Oregano, Salt and pepper, and lemon Zest'
+				},
+				{
+					instruction:
+						'Marinate Octopus in Olive Oil, Sherry Vinegar, Salt and Pepper'
+				},
+				{ instruction: 'Warm Romesco sauce and spread 2oz on plate' },
+				{
+					instruction:
+						'Strain and Saute Chickpeas on low for 6 min, then plate on top of Romesco'
+				},
+				{
+					instruction:
+						'Flash Grill Octopus to texture, plate on top of Chickpeas'
+				},
+				{
+					instruction: 'Garnish with Pickled Pepper relish and Maldon Sea Salt'
+				}
+			]);
+		});
 };
