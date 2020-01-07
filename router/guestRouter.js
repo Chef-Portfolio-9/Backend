43176877dfa12d4839a624ffeda47108692f6336
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-01-06 18:21:48 -0800

    Create endpoint to get chef's recipes.

--- router/guestRouter.js
+++ router/guestRouter.js
@@ -5,50 +5,73 @@ const guestDb = require('../models/guestDb');
 
 //Global GET
 router.get('/dish/', (req, res) => {
-    guestDb.get()
-    .then(dish => {
-        res.status(200).json(dish)
-    })
-    .catch(err => {
-        res.status(500).json({message: 'Error getting the recipes.'})
-    })
+	guestDb
+		.get()
+		.then(dish => {
+			res.status(200).json(dish);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Error getting the recipes.' });
+		});
 });
 
 //GET by id
 router.get('/dish/:id', validateRecipeId, (req, res) => {
-    const id = req.params.id
+	const id = req.params.id;
 
-    guestDb.getById(id)
-    .then(found => {
-        res.status(200).json(found)
-    })
-    .catch(err => {
-        res.status(500).json({message: 'Error finding that recipe.', err})
-    })
+	guestDb
+		.getById(id)
+		.then(found => {
+			res.status(200).json(found);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Error finding that recipe.', err });
+		});
 });
 
 //Global GET
 router.get('/chef/', (req, res) => {
-    guestDb.find()
-    .then(chefs => {
-        res.status(200).json(chefs)
-    })
-    .catch(err => {
-        res.status(500).json({message: 'Error finding the chefs.', err})
-    })
+	guestDb
+		.find()
+		.then(chefs => {
+			res.status(200).json(chefs);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Error finding the chefs.', err });
+		});
 });
 
 //GET by id
 router.get('/chef/:id', validateChefId, (req, res) => {
-    const id =req.params.id
+	const id = req.params.id;
 
-    guestDb.findBy(id)
-    .then(found => {
-        res.status(200).json(found)
-    })
-    .catch(err => {
-        res.status(500).json({message: 'Error getting that chef.', err})
-    })
+	guestDb
+		.findBy(id)
+		.then(found => {
+			res.status(200).json(found);
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Error getting that chef.', err });
+		});
+});
+
+// GET by id for recipes
+
+router.get('/chef/:id/recipes', (req, res) => {
+	const id = req.params.id;
+
+	guestDb
+		.findRecipes(id)
+		.then(dishes => {
+			if (dishes.length) {
+				res.status(200).json(dishes);
+			} else {
+				res.status(404).json({ message: 'Could not find the Chefs recipes. ' });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ message: 'Failed to get the chefs recipes', err });
+		});
 });
 
 // custom middleware
@@ -67,14 +90,15 @@ function validateRecipeId(req, res, next) {
 }
 
 function validateChefId(req, res, next) {
-    const id = req.params.id
+	const id = req.params.id;
 
-    guestDb.findBy(id)
-    .then(() => {
-        next()
-    })
-    .catch(err => {
-        res.status(404).json({message: 'Invalid chef id.', err})
-    })
+	guestDb
+		.findBy(id)
+		.then(() => {
+			next();
+		})
+		.catch(err => {
+			res.status(404).json({ message: 'Invalid chef id.', err });
+		});
 }
 module.exports = router;
