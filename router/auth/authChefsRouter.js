31e9f9d1591918a7602c7cecc5fe2c3a141371b3
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-01-06 19:01:46 -0800

    Refactor chef register endpoint.

--- router/auth/authChefsRouter.js
+++ router/auth/authChefsRouter.js
@@ -1,22 +1,20 @@
 const router = require('express').Router();
 
-
 const bcrypt = require('bcryptjs');
 
 // pull in signToken
 const signToken = require('../../JWT/signToken');
 
 //Pull in knex helper models
-const  chefsDb = require('../../models/authModels/chefsAuthDb');
+const chefsDb = require('../../models/authModels/chefsAuthDb');
 
 router.post('/register', (req, res) => {
-  // implement registration
-  let user = req.body;
+	// implement registration
+	let user = req.body;
 
 	const hash = bcrypt.hashSync(user.password, 8);
 
 	user.password = hash;
-
 
 	chefsDb
 		.add(user)
@@ -26,31 +24,33 @@ router.post('/register', (req, res) => {
 		.catch(err => {
 			res.status(500).json(err);
 		});
-
 });
 
 router.post('/login', (req, res) => {
-  // implement login
-  let { username, password } = req.body;
-  
-	chefsDb.getBy({ username })
-	  .first()
-	  .then(user => {
-		if (user && bcrypt.compareSync(password, user.password)) {
-		  // sign token
-		  const token = signToken(user); 
-  
-		  // send the token
-		  res.status(201).json({
-			welcome:`${user.full_name}!`, id: `${user.id}`, token
-		  });
-		} else {
-		  res.status(401).json({ message: "Invalid Credentials" });
-		}
-	  })
-	  .catch(error => {
-		res.status(500).json({message: "There was an error logging in",   error});
-	  });
+	// implement login
+	let { username, password } = req.body;
+
+	chefsDb
+		.getBy({ username })
+		.first()
+		.then(user => {
+			if (user && bcrypt.compareSync(password, user.password)) {
+				// sign token
+				const token = signToken(user);
+
+				// send the token
+				res.status(201).json({
+					welcome: `${user.full_name}!`,
+					id: `${user.id}`,
+					token
+				});
+			} else {
+				res.status(401).json({ message: 'Invalid Credentials' });
+			}
+		})
+		.catch(error => {
+			res.status(500).json({ message: 'There was an error logging in', error });
+		});
 });
 
 module.exports = router;
