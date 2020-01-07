4122ce60647849de93bad12140fe10568b884a1e
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-01-07 14:48:43 -0800

    Create unit tests for authChefRouter.

--- knexfile.js
+++ knexfile.js
@@ -18,7 +18,7 @@ module.exports = {
 	testing: {
 		client: 'sqlite3',
 		connection: {
-			filename: './database/test.db3'
+			filename: './data/test.db3'
 		},
 		useNullAsDefault: true,
 		migrations: {
