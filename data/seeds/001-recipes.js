cc31b30d100eabf1bb4161c76c065059cdd7ca74
Jeremy McWilliams <j2macwilliams@gmail.com>
2020-01-09 14:58:33 -0800

    Add del to seeds.

--- data/seeds/001-recipes.js
+++ data/seeds/001-recipes.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('recipes').truncate()
+  return knex('recipes').del()
     .then(function () {
       // Inserts seed entries
       return knex('recipes').insert([
